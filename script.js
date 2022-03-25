6619cf8e186fc897513198ace88a41b437a781d2
Kanhaiya Mishra <87825281+kanha996@users.noreply.github.com>
2022-03-25 18:18:42 +0000

    Finished

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const closebtn = document.querySelector('#close-btn');
 
 
 modalbtn.addEventListener('click',()=>{
-  document.querySelector
+  document.querySelector('#modal-body').style.display = 'grid';
+})
+
+closebtn.addEventListener('click',()=>{
+  document.querySelector('#modal-body').style.display = 'none';
 })
