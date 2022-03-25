9a1f0df36f089a29a7883955b6414469886286df
Kanhaiya Mishra <87825281+kanha996@users.noreply.github.com>
2022-03-25 18:06:43 +0000

    HTML CSS Complete

---
+++ script.js
@@ -0,0 +1,7 @@
+const modalbtn = document.querySelector('#modal-btn');
+const closebtn = document.querySelector('#close-btn');
+
+
+modalbtn.addEventListener('click',()=>{
+  document.querySelector
+})
